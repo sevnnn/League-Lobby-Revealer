3fa1d82bcb2c460a91cefcc64ea99cee77796c61
llllllllllllllll <llllllllllllllll>
2023-04-30 18:01:11 +0200

    adding in champselect check

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from src.Communicators.RiotClientCommunicator import RiotClientCommunicator
 from src.Factories.PlayerInfoFactory import PlayerInfoFactory
 
 lcu = LCUCommunicator()
+
+# prob will need to add "InGame" to this if, but need to test what the chat endpoint will return when ingame
+if lcu.get_current_client_phase() != "ChampSelect":
+    print("[ERR] Not in champion select")
+    exit(1)
+
 riot_client = RiotClientCommunicator()
 data_dragon = DataDragonCommunicator()
 
 
-for player in PlayerInfoFactory(lcu, riot_client, data_dragon).getInfo():
-    player.printInfo()
+for player in PlayerInfoFactory(lcu, riot_client, data_dragon).get_info():
+    player.print_info()
