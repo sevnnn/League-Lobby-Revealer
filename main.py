3642029111b828260f173637c87ee1b92f606695
llllllllllllllll <llllllllllllllll>
2023-05-01 14:24:12 +0200

    code style

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from urllib.parse import quote
 lcu = LCUCommunicator()
 
 # will handle "InGame" in another issue
-if lcu.get_current_client_phase() != "\"ChampSelect\"":
+if lcu.get_current_client_phase() != '"ChampSelect"':
     print("[ERR] Not in champion select")
     exit(1)
 
@@ -16,10 +16,10 @@ data_dragon = DataDragonCommunicator()
 
 url_safe_player_names: list[str] = []
 for player in PlayerInfoFactory(lcu, riot_client, data_dragon).get_info():
-    url_safe_player_names.append(
-        quote(player.username)
-    )
+    url_safe_player_names.append(quote(player.username))
     player.print_info()
 
-print(f"Multisearch url:\n"
-      f"https://www.op.gg/multisearch/{lcu.get_current_player_server()}?summoners={','.join(url_safe_player_names)}")
+print(
+    f"Multisearch url:\n"
+    f"https://www.op.gg/multisearch/{lcu.get_current_player_server()}?summoners={','.join(url_safe_player_names)}"
+)
